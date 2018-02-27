141c2e9a4bcf2c899df7557143882ed6bcf0597c
Alex Filip <acf61@cam.ac.uk>
2018-02-27 22:35:22 +0000

    Selected:1 bug fixed

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ MODELAPITHREADS = []
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('main_screen.html', itemslist=AIRCRAFTLIST)
+    return render_template('main_screen.html', itemslist=AIRCRAFTLIST, aircraftSelected=AIRCRAFT)
 
 @app.route('/about', methods=['GET'])
 def about():
